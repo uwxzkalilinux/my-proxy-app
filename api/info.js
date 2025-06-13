2162c8f164fe94ebe0b3fa2458d14779491606f1
uwxzkalilinux <115243597+uwxzkalilinux@users.noreply.github.com>
2025-06-13 20:50:37 +0300

    Update info.js

--- api/info.js
+++ api/info.js
@@ -9,8 +9,9 @@ export default async function handler(req, res) {
     const response = await fetch(`http://176.241.95.201:8092/id?unique=${unique}`, {
       method: "GET",
       headers: {
-        Authorization: "Basic " + Buffer.from("admin:241067890").toString("base64"),
+        "Authorization": "Basic YWRtaW46MjQxMDY3ODkw"
       },
+      redirect: "follow"
     });
 
     if (!response.ok) {
@@ -18,9 +19,10 @@ export default async function handler(req, res) {
     }
 
     const data = await response.json();
-    res.status(200).json(data);
+    return res.status(200).json(data);
+
   } catch (error) {
-    console.error("Proxy Error:", error);
-    res.status(500).json({ error: "حدث خطأ داخلي في الخادم" });
+    console.error("Error fetching data:", error);
+    return res.status(500).json({ error: "حدث خطأ داخلي" });
   }
 }
